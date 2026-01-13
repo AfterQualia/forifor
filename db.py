9026262e0c95ab06663b4c8ce4dcffd1d2c9c37c
AfterQualia <vonaviartem151@gmail.com>
2026-01-13 19:08:13 +0300

    Удалили файл бд

--- db.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(database = "foriforDB", user = "postgres", password = "")
-cur = conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS saved_info ("
-            "id SERIAL PRIMARY KEY,"
-            "hash TEXT,"
-            "site TEXT)")
-conn.commit()
-cur.close()
